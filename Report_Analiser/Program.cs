5821aaac546948b0cc96ff7a3ca206beac847eed
Mordy Pesach <mordypassover@gmail.com>
2026-06-24 11:29:27 +0300

    add project biuld, load text met, and started main array struct

--- Report_Analiser/Program.cs
+++ Report_Analiser/Program.cs
@@ -1,1 +1,68 @@
-﻿
+﻿using System;
+using System.IO;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Analiser
+    {
+    public enum reportType
+        {
+            Intel,
+            Recon,
+            Analyze,
+            Collect
+        }
+    public enum Status
+        {
+        Rejected,
+        Approved,
+        Pending
+        }
+    class ReportAnaliser
+        {
+        static string[]? ReadTxtFile(string path )
+        {
+             
+            if (!File.Exists(path)) { return null; }
+
+            string[] fileData = File.ReadAllLines(path, Encoding.UTF8);
+            return fileData;
+        }
+        static void ProcessReports(string[] stringDataArr, string[] unitName, string[] reportType, double[] score, string[] status)
+        {
+        
+        }
+
+        static void CalculateAverage() { }
+
+        static void FindMaxScore() { }
+
+        static void FindMinScore() { }
+
+        static void CountByStatus() { }
+
+        static void CountByType() { }
+
+        static void DisplayBasicStatistics() { }
+
+        static void DisplayStatusCounts() { }
+
+        static void DisplayTypeCounts() { }
+        static void DisplayHighestPriorityApproved() { }
+        static void DisplayAverageByPriority() { }
+
+        static void Main()
+            {
+            string filePath = "txt.reports";
+            string[]? fileData = ReadTxtFile(filePath);
+            if (fileData != null)
+            {
+                string[] unitName = new string[fileData.Length];
+                string[] reportType = new string[fileData.Length];
+                int[] priority = new int[fileData.Length];
+                double[] score = new double[fileData.Length];
+                string[] status = new string[fileData.Length];
+            }
+            }   
+        }
+    }
